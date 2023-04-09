32bb2e5f9bcc0871adcbb4489bac3d7514e76220
James Park <james@found.app>
2023-04-09 16:13:21 -0700

    include ts declaration files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,9 @@
 import terser from '@rollup/plugin-terser';
-import pkg from './package.json';
+import pkg from './package.json'  assert { type: "json" };
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   plugins: [
     typescript(),
     terser(),
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import {
   getSearchSelectors
 } from './selectors'
 import { search } from './actions'
+// @ts-ignore
 import reduxSearch from './reduxSearch'
+// @ts-ignore
 import reducer from './reducer'
+// @ts-ignore
 import SearchApi from './SearchApi'
 
 export default reduxSearch
@@ -16,4 +19,5 @@ export {
   search as createSearchAction,
   SearchApi
 }
+// @ts-ignore
 export { INDEX_MODES } from 'js-worker-search'
