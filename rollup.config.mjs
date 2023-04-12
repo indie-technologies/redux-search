b116d2bfa317125be1e04911e268eda3a0108887
James Park <james@found.app>
2023-04-12 12:40:15 -0700

    just do commonjs for now, we are not ready for esm

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,21 @@
 import terser from '@rollup/plugin-terser';
 import pkg from './package.json'  assert { type: "json" };
 import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/index.ts',
   plugins: [
+    nodeResolve(),
+    commonjs(),
     typescript(),
     terser(),
   ],
   output: [
     {
-      file: pkg.module,
-      format: 'es' 
+      file: pkg.main,
+      format: 'cjs',
     },
   ],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,13 @@
-import {
-  defaultSearchStateSelector,
-  getSearchSelectors
-// @ts-ignore
-} from './selectors'
 // @ts-ignore
 import { search } from './actions'
 // @ts-ignore
 import reduxSearch from './reduxSearch'
 // @ts-ignore
 import reducer from './reducer'
-// @ts-ignore
-import SearchApi from './SearchApi'
 
 export default reduxSearch
 export {
-  defaultSearchStateSelector,
-  getSearchSelectors,
   reducer,
   reduxSearch,
   search as createSearchAction,
-  SearchApi
 }
-// @ts-ignore
-export { INDEX_MODES } from 'js-worker-search'
